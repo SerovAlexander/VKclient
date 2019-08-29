a7ccb9a12bf5802a659e604e78f684e7f00735a2
Aleksandr <Serov_ae@list.ru>
2019-08-29 15:53:51 +0300

    Добавил кнобку лайк и лейбл с количеством лайков

---
+++ VKClient/Cell/FriendXIBCell.swift
@@ -0,0 +1,18 @@
+//
+//  FriendXIBCell.swift
+//  VKClient
+//
+//  Created by Aleksandr Serov on 29/08/2019.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import UIKit
+
+class FriendXIBCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}

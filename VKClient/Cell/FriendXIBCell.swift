163acd6bcf146993f4c1bf49281319458d367504
Aleksandr <Serov_ae@list.ru>
2019-08-30 15:41:35 +0300

    Изменил метод подсчета лайков

--- VKClient/Cell/FriendXIBCell.swift
+++ VKClient/Cell/FriendXIBCell.swift
@@ -10,9 +10,47 @@ import UIKit
 
 class FriendXIBCell: UICollectionViewCell {
 
+   @IBOutlet weak var avatar: UIImageView!
+   @IBOutlet weak var heartImageView: UIImageView!
+    @IBOutlet weak var likeCountLabel: UILabel!
+
+    var likeCount = 0
+    
+    
+    public var heartFilled = false {
+        didSet {
+            if heartFilled {
+                if likeCount != 0 {
+                likeCount -= 1
+                } else {}
+                likeCountLabel.text = String(likeCount)
+                heartImageView.image = UIImage(named: "EmptyHeart")
+                
+            } else {
+                likeCount += 1
+                heartImageView.image = UIImage(named: "FullHeart")
+                likeCountLabel.text = String(likeCount)
+            }
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+         likeCountLabel.text = "0"
+        heartImageView.image = UIImage(named: "EmptyHeart")
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(heartTapped))
+        heartImageView.addGestureRecognizer(tapGestureRecognizer)
     }
-
+    
+    @objc private func heartTapped() {
+        heartFilled.toggle()
+    }
+    
+// Метод, чтобы ячейки при переиспользовании всегда появлялись с без лайка
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        heartImageView.image = UIImage(named: "EmptyHeart")
+    }
 }

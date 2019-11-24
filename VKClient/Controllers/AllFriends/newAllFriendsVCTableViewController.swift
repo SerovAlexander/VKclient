30a86029badbbcb0f8ef982f88ef01aa65541c66
Aleksandr <Serov_ae@list.ru>
2019-11-24 20:00:53 +0300

    Реализация обновления контроллера при получении Notifications

--- VKClient/Controllers/AllFriends/newAllFriendsVCTableViewController.swift
+++ VKClient/Controllers/AllFriends/newAllFriendsVCTableViewController.swift
@@ -11,54 +11,61 @@ import RealmSwift
 
 class newAllFriendsVCTableViewController: UITableViewController {
 
-    var users = [User]()
+//    var users = [User]()
     var token: NotificationToken?
 
-//    private lazy var users = try? Realm().objects(User.self).sorted(byKeyPath: "id")
+    private lazy var users = try? Realm().objects(User.self).sorted(byKeyPath: "id")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5){
+        let realm = try! Realm()
+        let users = realm.objects(User.self)
+        self.token = users.observe { change in
+            switch change {
+                case .initial(let result):
+                    self.tableView.reloadData()
+//                    print(result)
+                case let .update(results, indexesDelete, indexesInsert, Indexesmodifications):
+                    self.tableView.beginUpdates()
+                        self.tableView.insertRows(at: indexesInsert.map{IndexPath(row: $0, section: 0)}, with: .none)
+                        self.tableView.deleteRows(at: indexesDelete.map{IndexPath(row: $0, section: 0)}, with: .none)
+                        self.tableView.reloadRows(at: Indexesmodifications.map{IndexPath(row: $0, section: 0)}, with: .none)
+                    self.tableView.endUpdates()
+                print(indexesDelete)
+                print(indexesInsert)
+                print(Indexesmodifications)
+                case .error:
+                    print("error")
+                           }
+               
+                   }
+        }
         NetworkService.loadFriends { [weak self] result in
             guard let self = self else { return }
             switch result {
             case .success(let users):
                 DataBase.save(items: users)
                 self.users = users
-                self.tableView.reloadData()
+//                self.tableView.reloadData()
             case .failure(let error):
                 fatalError(error.localizedDescription)
             }
         }
-        let realm = try! Realm()
-            let users = realm.objects(User.self)
-            self.token = users.observe { change in
-                switch change {
-                    case .initial(let result):
-                        print(result)
-                    case let .update(results, indexesDelete, indexesInsert, Indexesmodifications):
-//                        print(results)
-                        print(indexesDelete)
-                        print(indexesInsert)
-                        print(Indexesmodifications)
-                    case .error: print("error")
-                                        
-                    }
         
-            }
     }
 
        // MARK: - Table view data source
      override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
          // #warning Incomplete implementation, return the number of rows
-        return users.count
-//        return users?.count ?? 0
+//        return users.count
+       return users?.count ?? 0
      }
      
      override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
          let cell = tableView.dequeueReusableCell(withIdentifier: "AllFriendCell", for: indexPath) as! AllFriendCell
-        let user = users[indexPath.row]
-//        guard let user = users?[indexPath.row] else {return cell}
+//        let user = users[indexPath.row]
+        guard let user = users?[indexPath.row] else {return cell}
          cell.configure(with: user)
 
          return cell

351898317360cd0b88c2c7082492da0eaf1ee5a5
Aleksandr <Serov_ae@list.ru>
2019-12-02 20:57:32 +0300

    Реализовал Realm Notifications класса Item(Group)

--- VKClient/Controllers/MyGroups/NewMyGroupVC.swift
+++ VKClient/Controllers/MyGroups/NewMyGroupVC.swift
@@ -13,12 +13,35 @@ class NewMyGroupVC: UITableViewController {
     
     
 //    var newGroup = [Items]()
+    var token: NotificationToken?
 
     private lazy var newGroup = try? Realm().objects(Items.self)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//Вопросы!!!
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2){
+        let realm = try! Realm()
+        let newGroup = realm.objects(Items.self)
+        self.token = newGroup.observe { change in
+        switch change {
+            case .initial(let result):
+                self.tableView.reloadData()
+                print(result)
+            case let .update(results, indexesDelete, indexesInsert, Indexesmodifications):
+                self.tableView.beginUpdates()
+                    self.tableView.insertRows(at: indexesInsert.map{IndexPath(row: $0, section: 0)}, with: .none)
+                    self.tableView.deleteRows(at: indexesDelete.map{IndexPath(row: $0, section: 0)}, with: .none)
+                    self.tableView.reloadRows(at: Indexesmodifications.map{IndexPath(row: $0, section: 0)}, with: .none)
+                self.tableView.endUpdates()
+            print(indexesDelete)
+            print(indexesInsert)
+            print(Indexesmodifications)
+            case .error:
+                print("error")
+                        }
+                    }
+               }
+
        NetworkService.getGroup { [weak self] result in
            guard let self = self else { return }
            switch result {

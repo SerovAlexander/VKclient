9d4775888148306fd3980b6e7b9808f6be631238
Aleksandr <Serov_ae@list.ru>
2020-03-04 15:46:19 +0500

    Реализация нового контроллера для вывода новостей с 4 протатипами ячеек

--- VKClient/Controllers/newNews/NewsFooterCell.swift
+++ VKClient/Controllers/newNews/NewsFooterCell.swift
@@ -7,11 +7,33 @@
 //
 
 import UIKit
+import RealmSwift
+
 
 class NewsFooterCell: UITableViewCell {
 
+//    private lazy var news =  try? Realm().objects(News.self)
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var likeCount: UILabel!
+    private lazy var newss =  try? Realm().objects(News.self)
+    var news = News()
+    var likeState = false
+    
+    
     @IBAction func setLike(_ sender: Any) {
-        (sender as! ButtonLike).liked = !(sender as! ButtonLike).liked
+        var likes = news.likesCount
+    if !likeState {
+               likeButton.setImage(UIImage(named: "FullHeart"), for: UIControl.State.normal)
+                  likes = likes + 1
+                  self.likeCount.text = String(likes)
+               likeState.toggle()
+       
+           }else {
+               likeButton.setImage(UIImage(named: "EmptyHeart"), for: UIControl.State.normal)
+               likeState.toggle()
+
+           }
+        
     }
     
     override func awakeFromNib() {
@@ -19,10 +41,11 @@ class NewsFooterCell: UITableViewCell {
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    public func configure(with news: News?) {
+        
+        likeCount.text = String(news?.likesCount ?? 0)
 
-        // Configure the view for the selected state
-    }
-
-}
+        }
+    
+     }
+ 
